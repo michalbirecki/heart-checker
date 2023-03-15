8ccb381064d3526c128a5470d359c433e29f3b52
Ricard Torres <revenuelegion@gmail.com>
2023-03-15 11:59:35 +0100

    Better error logging for pagerduty code 2

--- pages/api/checker/index.js
+++ pages/api/checker/index.js
@@ -84,10 +84,10 @@ export default async function handler(req, res) {
     //     });
     // }
 
-    res.status(200).json({
-      isOk: isOk,
-      message: "None of the cinditions triggered, so function simply returned",
-    }); // out of bounds gave this. something must have failed in
+    // res.status(200).json({
+    //   isOk: isOk,
+    //   message: "None of the cinditions triggered, so function simply returned",
+    // }); // out of bounds gave this. something must have failed in
   } else {
     res.status(200).json({ message: "Did not receive POST request" });
   }
